23467f76b4bf49818347b97f1da836c345888c9f
aanu20 <aanu84962@gmail.com>
2025-01-19 22:16:08 +0530

    images improved

--- youtube-react/src/TopBarFolder/TopBar.jsx
+++ youtube-react/src/TopBarFolder/TopBar.jsx
@@ -1,11 +1,17 @@
-import youtubeLogo from "./images/youtube-fill.png";
+//import youtubeLogo from "./images/youtube-fill.png";
+//import sidebar from "./images/side-bar-fill.png";
+//it is multiple steps so include index.js in image and export all the images
+import { youtubeLogo,sidebar,mic,loginbar } from "./images"
 function TopBar(){
     return (
-    <>
+    <>  
+        <img id= "side-bar-icon" src={sidebar}></img>
         <img id="youtube-logo" src={youtubeLogo}></img>
         <h5>YouTube</h5>
         <input type="text" placeholder="search" name="search-name" ></input>
+        <img id="mic-icon" src={mic}></img>
         <button id="create-button">Create</button>
+        <img id="login-icon" src={loginbar}></img>
     </>
     )
 }
---
+++ youtube-react/src/TopBarFolder/images/index.js
@@ -0,0 +1,4 @@
+export { default as youtubeLogo } from "./youtube-fill.png";
+export { default as sidebar } from "./side-bar-fill.png";
+export {default as mic} from "./mic-line.png";
+export {default as loginbar} from "./login-box-line.png";
