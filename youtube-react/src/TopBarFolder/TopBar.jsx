e971aa534cae783dbcd419e64df4c033b7604a03
aanu20 <aanu84962@gmail.com>
2025-01-21 22:31:25 +0530

    top bar

--- youtube-react/src/TopBarFolder/TopBar.jsx
+++ youtube-react/src/TopBarFolder/TopBar.jsx
@@ -2,17 +2,26 @@
 //import sidebar from "./images/side-bar-fill.png";
 //it is multiple steps so include index.js in image and export all the images
 import { youtubeLogo,sidebar,mic,loginbar } from "./images"
+import "./TopBar.css"
 function TopBar(){
     return (
     <>  
-        <div>    
+        <div className="Top">    
             <img id= "side-bar-icon" src={sidebar}></img>
             <img id="youtube-logo" src={youtubeLogo}></img>
-            <h5>YouTube</h5>
-            <input type="text" placeholder="search" name="search-name" ></input>
+            <span >YouTube</span>
+            <input id="search-bar" type="text" placeholder="search" name="search-name" ></input>
             <img id="mic-icon" src={mic}></img>
             <button id="create-button">Create</button>
             <img id="login-icon" src={loginbar}></img>
+        </div>
+
+        <div>
+            <button>All</button>
+            <button>Music</button>
+            <button>courses</button>
+            <button>podcasts</button>
+            <button>Tamil Cinema</button>
         </div>
     </>
     )
